306525608a22e66c651d8f5e611e4b9b3258c48a
Coryf65 <Coryf65@users.noreply.github.com>
2024-11-29 11:04:50 -0600

    limiting FPS to 60

--- main.py
+++ main.py
@@ -6,12 +6,17 @@
 import pygame
 from constants import *
 
+ONE_SECOND = 1000
+
 def main():
     pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    # setup
+    clock = pygame.time.Clock()
+    delta_time = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -21,6 +26,7 @@ def main():
         
         screen.fill(color=(0,0,0))        
         pygame.display.flip() #last to update screen
+        delta_time = clock.tick(TARGET_FPS) / ONE_SECOND
 
 
 if __name__ == "__main__":
