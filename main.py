80e35d9f3ba425fc94e7b2b24a04eae93c9026b0
Coryf65 <Coryf65@users.noreply.github.com>
2024-11-29 12:30:50 -0600

    drawing the player on the screen

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 ONE_SECOND = 1000
 
@@ -15,17 +16,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     # setup
-    clock = pygame.time.Clock()
-    delta_time = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player1 = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    delta_time = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return # allows the close button to work
         
-        screen.fill(color=(0,0,0))        
+        screen.fill(color=(0,0,0))
+        player1.draw(screen)
         pygame.display.flip() #last to update screen
+        
+        # limit to 60 fps
         delta_time = clock.tick(TARGET_FPS) / ONE_SECOND
 
 
